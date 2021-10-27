de2e23af0b84ea32bbe414c25508cd1ba5f3b993
Sanketkumar Patel <W0776059@myscc.ca>
2021-10-27 10:28:10 -0400

    Modifed the bootstrap

--- SanketBookStore/Startup.cs
+++ SanketBookStore/Startup.cs
@@ -32,7 +32,7 @@ namespace SanketBookStore
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
         }
